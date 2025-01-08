1a1800742f038966b14d8b3e383260b9d0e666a3
Roberto Antonio Navarro Estrada <147531709+Antxnszn@users.noreply.github.com>
2025-01-08 02:09:05 -0600

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,7 @@ import pymysql
 conn = pymysql.connect(
     host="localhost",
     user="root",
-    password="sancochito2",
+    password="",
     database="escomcarreras",
     charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor
